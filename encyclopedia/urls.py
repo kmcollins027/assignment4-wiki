f500aebe79ece560b234c86796ac80054545b177
Kevin <kmcollins3@alaska.edu>
2022-09-29 14:35:42 -0800

    added code for create_page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:TITLE>", views.entry_page, name="TITLE")
+    path("wiki/<str:TITLE>", views.entry_page, name="TITLE"),
+    path("wiki/create_page", views.create_page, name="create_page"),
+    
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.files.storage import default_storage
+
 
 from . import util
 
@@ -21,4 +23,16 @@ def entry_page(request, TITLE):
     else:
         MD_to_HTML = md.convert(html_contents)
         return render(request, "encyclopedia/show_entry.html", {"entry_contents": MD_to_HTML, "entry_name": entry_name})
+
+def create_page(request):
+    entry = request.GET["entry_name"]
+    text_contents = request.GET["contents"]
+
+    if default_storage.exists(entry):
+        render(request, "encyclopedia.index.html", {"errmsg": "Page already exists"})
+    else:
+        util.save_entry(entry, text_contents)
+        html_contents = util.get_entry(entry)
+        MD_to_HTML = md.convert(html_contents)
+        render(request, "encyclopedia/show_entry.html", {"entry_contents": MD_to_HTML, "entry_name": entry})
 
